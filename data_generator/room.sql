967bdcb620f606093fdb635e293de41c2d9a85fa
Andrew <a.ermak@innopolis.ru>
2018-04-11 21:05:26 +0300

    add sqls

---
+++ data_generator/room.sql
@@ -0,0 +1,262 @@
+INSERT INTO room VALUES (DEFAULT, '1', '4', '31', '1', 'ut rhoncus',
+                         'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum',
+                         '2023');
+INSERT INTO room
+VALUES (DEFAULT, '1', '9', '8', '2', 'sit', 'in imperdiet et commodo vulputate justo in blandit ultrices enim', '2624');
+INSERT INTO room VALUES (DEFAULT, '1', '3', '22', '9', 'iaculis',
+                         'pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate', '2934');
+INSERT INTO room VALUES (DEFAULT, '1', '2', '6', '4', 'ut erat',
+                         'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula',
+                         '1221');
+INSERT INTO room VALUES
+  (DEFAULT, '1', '16', '28', '4', 'et ultrices', 'massa donec dapibus duis at velit eu est congue elementum in hac',
+   '1634');
+INSERT INTO room VALUES (DEFAULT, '1', '16', '30', '4', 'justo morbi',
+                         'arcu libero rutrum ac lobortis vel dapibus at diam nam tristique tortor eu', '2123');
+INSERT INTO room VALUES
+  (DEFAULT, '1', '8', '10', '6', 'donec', 'aliquam erat volutpat in congue etiam justo etiam pretium iaculis', '1832');
+INSERT INTO room VALUES (DEFAULT, '1', '13', '22', '2', 'ipsum',
+                         'donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet',
+                         '1373');
+INSERT INTO room VALUES (DEFAULT, '1', '7', '1', '5', 'adipiscing',
+                         'ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient',
+                         '2629');
+INSERT INTO room VALUES (DEFAULT, '1', '13', '13', '6', 'neque',
+                         'vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis',
+                         '2131');
+INSERT INTO room VALUES (DEFAULT, '2', '12', '9', '10', 'ut rhoncus',
+                         'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum',
+                         '2356');
+INSERT INTO room VALUES
+  (DEFAULT, '2', '11', '19', '10', 'sit', 'in imperdiet et commodo vulputate justo in blandit ultrices enim', '1851');
+INSERT INTO room VALUES
+  (DEFAULT, '2', '1', '5', '7', 'iaculis', 'pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate',
+   '1984');
+INSERT INTO room VALUES (DEFAULT, '2', '6', '7', '1', 'ut erat',
+                         'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula',
+                         '1712');
+INSERT INTO room VALUES
+  (DEFAULT, '2', '5', '23', '5', 'et ultrices', 'massa donec dapibus duis at velit eu est congue elementum in hac',
+   '2575');
+INSERT INTO room VALUES (DEFAULT, '2', '5', '18', '10', 'justo morbi',
+                         'arcu libero rutrum ac lobortis vel dapibus at diam nam tristique tortor eu', '2239');
+INSERT INTO room VALUES
+  (DEFAULT, '2', '16', '28', '7', 'donec', 'aliquam erat volutpat in congue etiam justo etiam pretium iaculis', '1701');
+INSERT INTO room VALUES (DEFAULT, '2', '3', '11', '5', 'ipsum',
+                         'donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet',
+                         '1784');
+INSERT INTO room VALUES (DEFAULT, '2', '8', '28', '5', 'adipiscing',
+                         'ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient',
+                         '2249');
+INSERT INTO room VALUES (DEFAULT, '2', '16', '12', '7', 'neque',
+                         'vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis',
+                         '2316');
+INSERT INTO room VALUES (DEFAULT, '3', '2', '4', '10', 'ut rhoncus',
+                         'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum',
+                         '2642');
+INSERT INTO room VALUES
+  (DEFAULT, '3', '9', '1', '10', 'sit', 'in imperdiet et commodo vulputate justo in blandit ultrices enim', '2445');
+INSERT INTO room VALUES (DEFAULT, '3', '9', '18', '7', 'iaculis',
+                         'pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate', '2743');
+INSERT INTO room VALUES (DEFAULT, '3', '15', '13', '3', 'ut erat',
+                         'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula',
+                         '1954');
+INSERT INTO room VALUES
+  (DEFAULT, '3', '2', '5', '4', 'et ultrices', 'massa donec dapibus duis at velit eu est congue elementum in hac',
+   '2771');
+INSERT INTO room VALUES (DEFAULT, '3', '1', '18', '5', 'justo morbi',
+                         'arcu libero rutrum ac lobortis vel dapibus at diam nam tristique tortor eu', '2872');
+INSERT INTO room VALUES
+  (DEFAULT, '3', '7', '22', '6', 'donec', 'aliquam erat volutpat in congue etiam justo etiam pretium iaculis', '1339');
+INSERT INTO room VALUES (DEFAULT, '3', '4', '30', '6', 'ipsum',
+                         'donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet',
+                         '1819');
+INSERT INTO room VALUES (DEFAULT, '3', '3', '6', '8', 'adipiscing',
+                         'ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient',
+                         '1608');
+INSERT INTO room VALUES (DEFAULT, '3', '15', '13', '9', 'neque',
+                         'vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis',
+                         '2314');
+INSERT INTO room VALUES (DEFAULT, '4', '13', '17', '3', 'ut rhoncus',
+                         'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum',
+                         '1741');
+INSERT INTO room VALUES
+  (DEFAULT, '4', '16', '15', '5', 'sit', 'in imperdiet et commodo vulputate justo in blandit ultrices enim', '2667');
+INSERT INTO room VALUES (DEFAULT, '4', '10', '13', '3', 'iaculis',
+                         'pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate', '2767');
+INSERT INTO room VALUES (DEFAULT, '4', '14', '7', '9', 'ut erat',
+                         'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula',
+                         '2112');
+INSERT INTO room VALUES
+  (DEFAULT, '4', '6', '9', '5', 'et ultrices', 'massa donec dapibus duis at velit eu est congue elementum in hac',
+   '1076');
+INSERT INTO room VALUES (DEFAULT, '4', '2', '10', '10', 'justo morbi',
+                         'arcu libero rutrum ac lobortis vel dapibus at diam nam tristique tortor eu', '2408');
+INSERT INTO room VALUES
+  (DEFAULT, '4', '10', '26', '5', 'donec', 'aliquam erat volutpat in congue etiam justo etiam pretium iaculis', '1440');
+INSERT INTO room VALUES (DEFAULT, '4', '1', '13', '7', 'ipsum',
+                         'donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet',
+                         '1570');
+INSERT INTO room VALUES (DEFAULT, '4', '15', '12', '4', 'adipiscing',
+                         'ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient',
+                         '1347');
+INSERT INTO room VALUES (DEFAULT, '4', '1', '2', '6', 'neque',
+                         'vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis',
+                         '1118');
+INSERT INTO room VALUES (DEFAULT, '5', '10', '15', '2', 'ut rhoncus',
+                         'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum',
+                         '2631');
+INSERT INTO room VALUES
+  (DEFAULT, '5', '9', '31', '2', 'sit', 'in imperdiet et commodo vulputate justo in blandit ultrices enim', '1686');
+INSERT INTO room VALUES (DEFAULT, '5', '2', '32', '7', 'iaculis',
+                         'pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate', '2452');
+INSERT INTO room VALUES (DEFAULT, '5', '11', '8', '6', 'ut erat',
+                         'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula',
+                         '1329');
+INSERT INTO room VALUES
+  (DEFAULT, '5', '13', '1', '6', 'et ultrices', 'massa donec dapibus duis at velit eu est congue elementum in hac',
+   '1057');
+INSERT INTO room VALUES (DEFAULT, '5', '4', '23', '10', 'justo morbi',
+                         'arcu libero rutrum ac lobortis vel dapibus at diam nam tristique tortor eu', '2997');
+INSERT INTO room VALUES
+  (DEFAULT, '5', '4', '11', '4', 'donec', 'aliquam erat volutpat in congue etiam justo etiam pretium iaculis', '2351');
+INSERT INTO room VALUES (DEFAULT, '5', '16', '23', '9', 'ipsum',
+                         'donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet',
+                         '1927');
+INSERT INTO room VALUES (DEFAULT, '5', '13', '25', '5', 'adipiscing',
+                         'ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient',
+                         '1200');
+INSERT INTO room VALUES (DEFAULT, '5', '15', '11', '7', 'neque',
+                         'vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis',
+                         '2009');
+INSERT INTO room VALUES (DEFAULT, '6', '10', '2', '5', 'ut rhoncus',
+                         'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum',
+                         '1200');
+INSERT INTO room VALUES
+  (DEFAULT, '6', '12', '22', '9', 'sit', 'in imperdiet et commodo vulputate justo in blandit ultrices enim', '1583');
+INSERT INTO room VALUES (DEFAULT, '6', '5', '10', '8', 'iaculis',
+                         'pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate', '2392');
+INSERT INTO room VALUES (DEFAULT, '6', '9', '25', '10', 'ut erat',
+                         'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula',
+                         '1433');
+INSERT INTO room VALUES
+  (DEFAULT, '6', '15', '20', '1', 'et ultrices', 'massa donec dapibus duis at velit eu est congue elementum in hac',
+   '2391');
+INSERT INTO room VALUES (DEFAULT, '6', '13', '22', '9', 'justo morbi',
+                         'arcu libero rutrum ac lobortis vel dapibus at diam nam tristique tortor eu', '2225');
+INSERT INTO room VALUES
+  (DEFAULT, '6', '16', '8', '4', 'donec', 'aliquam erat volutpat in congue etiam justo etiam pretium iaculis', '2431');
+INSERT INTO room VALUES (DEFAULT, '6', '6', '21', '6', 'ipsum',
+                         'donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet',
+                         '2293');
+INSERT INTO room VALUES (DEFAULT, '6', '9', '30', '7', 'adipiscing',
+                         'ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient',
+                         '1358');
+INSERT INTO room VALUES (DEFAULT, '6', '7', '16', '10', 'neque',
+                         'vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis',
+                         '1461');
+INSERT INTO room VALUES (DEFAULT, '7', '13', '12', '9', 'ut rhoncus',
+                         'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum',
+                         '2133');
+INSERT INTO room VALUES
+  (DEFAULT, '7', '9', '10', '8', 'sit', 'in imperdiet et commodo vulputate justo in blandit ultrices enim', '2607');
+INSERT INTO room VALUES (DEFAULT, '7', '7', '29', '4', 'iaculis',
+                         'pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate', '2381');
+INSERT INTO room VALUES (DEFAULT, '7', '15', '23', '10', 'ut erat',
+                         'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula',
+                         '1075');
+INSERT INTO room VALUES
+  (DEFAULT, '7', '2', '14', '4', 'et ultrices', 'massa donec dapibus duis at velit eu est congue elementum in hac',
+   '2249');
+INSERT INTO room VALUES (DEFAULT, '7', '6', '4', '10', 'justo morbi',
+                         'arcu libero rutrum ac lobortis vel dapibus at diam nam tristique tortor eu', '1795');
+INSERT INTO room VALUES
+  (DEFAULT, '7', '3', '26', '2', 'donec', 'aliquam erat volutpat in congue etiam justo etiam pretium iaculis', '2021');
+INSERT INTO room VALUES (DEFAULT, '7', '7', '6', '7', 'ipsum',
+                         'donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet',
+                         '2077');
+INSERT INTO room VALUES (DEFAULT, '7', '7', '5', '5', 'adipiscing',
+                         'ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient',
+                         '2565');
+INSERT INTO room VALUES (DEFAULT, '7', '3', '8', '2', 'neque',
+                         'vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis',
+                         '1475');
+INSERT INTO room VALUES (DEFAULT, '8', '2', '19', '8', 'ut rhoncus',
+                         'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum',
+                         '1794');
+INSERT INTO room VALUES
+  (DEFAULT, '8', '2', '27', '5', 'sit', 'in imperdiet et commodo vulputate justo in blandit ultrices enim', '1383');
+INSERT INTO room VALUES (DEFAULT, '8', '3', '5', '10', 'iaculis',
+                         'pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate', '1677');
+INSERT INTO room VALUES (DEFAULT, '8', '16', '17', '6', 'ut erat',
+                         'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula',
+                         '1751');
+INSERT INTO room VALUES
+  (DEFAULT, '8', '6', '17', '4', 'et ultrices', 'massa donec dapibus duis at velit eu est congue elementum in hac',
+   '2131');
+INSERT INTO room VALUES (DEFAULT, '8', '8', '7', '5', 'justo morbi',
+                         'arcu libero rutrum ac lobortis vel dapibus at diam nam tristique tortor eu', '1828');
+INSERT INTO room VALUES
+  (DEFAULT, '8', '8', '5', '8', 'donec', 'aliquam erat volutpat in congue etiam justo etiam pretium iaculis', '2449');
+INSERT INTO room VALUES (DEFAULT, '8', '4', '8', '5', 'ipsum',
+                         'donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet',
+                         '1095');
+INSERT INTO room VALUES (DEFAULT, '8', '9', '28', '1', 'adipiscing',
+                         'ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient',
+                         '2582');
+INSERT INTO room VALUES (DEFAULT, '8', '4', '22', '3', 'neque',
+                         'vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis',
+                         '2941');
+INSERT INTO room VALUES (DEFAULT, '9', '1', '16', '4', 'ut rhoncus',
+                         'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum',
+                         '2891');
+INSERT INTO room VALUES
+  (DEFAULT, '9', '8', '11', '10', 'sit', 'in imperdiet et commodo vulputate justo in blandit ultrices enim', '2085');
+INSERT INTO room VALUES
+  (DEFAULT, '9', '1', '9', '8', 'iaculis', 'pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate',
+   '1168');
+INSERT INTO room VALUES (DEFAULT, '9', '14', '20', '6', 'ut erat',
+                         'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula',
+                         '1134');
+INSERT INTO room VALUES
+  (DEFAULT, '9', '12', '14', '2', 'et ultrices', 'massa donec dapibus duis at velit eu est congue elementum in hac',
+   '1663');
+INSERT INTO room VALUES (DEFAULT, '9', '2', '11', '9', 'justo morbi',
+                         'arcu libero rutrum ac lobortis vel dapibus at diam nam tristique tortor eu', '2568');
+INSERT INTO room VALUES
+  (DEFAULT, '9', '1', '17', '4', 'donec', 'aliquam erat volutpat in congue etiam justo etiam pretium iaculis', '2251');
+INSERT INTO room VALUES (DEFAULT, '9', '13', '32', '7', 'ipsum',
+                         'donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet',
+                         '2785');
+INSERT INTO room VALUES (DEFAULT, '9', '16', '24', '7', 'adipiscing',
+                         'ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient',
+                         '2302');
+INSERT INTO room VALUES (DEFAULT, '9', '6', '16', '5', 'neque',
+                         'vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis',
+                         '1089');
+INSERT INTO room VALUES (DEFAULT, '10', '16', '12', '4', 'ut rhoncus',
+                         'accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum',
+                         '2282');
+INSERT INTO room VALUES
+  (DEFAULT, '10', '12', '10', '7', 'sit', 'in imperdiet et commodo vulputate justo in blandit ultrices enim', '1588');
+INSERT INTO room VALUES (DEFAULT, '10', '6', '20', '8', 'iaculis',
+                         'pellentesque at nulla suspendisse potenti cras in purus eu magna vulputate', '2938');
+INSERT INTO room VALUES (DEFAULT, '10', '12', '11', '10', 'ut erat',
+                         'nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula vehicula',
+                         '1187');
+INSERT INTO room VALUES
+  (DEFAULT, '10', '12', '19', '1', 'et ultrices', 'massa donec dapibus duis at velit eu est congue elementum in hac',
+   '2749');
+INSERT INTO room VALUES (DEFAULT, '10', '14', '18', '10', 'justo morbi',
+                         'arcu libero rutrum ac lobortis vel dapibus at diam nam tristique tortor eu', '2310');
+INSERT INTO room VALUES
+  (DEFAULT, '10', '5', '24', '4', 'donec', 'aliquam erat volutpat in congue etiam justo etiam pretium iaculis', '2315');
+INSERT INTO room VALUES (DEFAULT, '10', '11', '26', '10', 'ipsum',
+                         'donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam sit amet',
+                         '2519');
+INSERT INTO room VALUES (DEFAULT, '10', '5', '2', '9', 'adipiscing',
+                         'ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque penatibus et magnis dis parturient',
+                         '1163');
+INSERT INTO room VALUES (DEFAULT, '10', '7', '31', '7', 'neque',
+                         'vestibulum quam sapien varius ut blandit non interdum in ante vestibulum ante ipsum primis',
+                         '2257');
