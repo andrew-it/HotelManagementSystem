ddc549902dc3ebc9ac6bd0c419c24789920bea0a
Aljutor <aljutor@gmail.com>
2018-04-25 17:29:09 +0300

    Support of Dockerfile, new Jenkinsfile

--- test/unit/test_models.py
+++ test/unit/test_models.py
@@ -1,5 +1,4 @@
 import nose
-
 from app.models import User, AnonymousUser, Customer, HotelAdmin
 
 
@@ -12,52 +11,52 @@ def test_user():
 
     with nose.allure.step('init user'):
         user = User(id, email, password, role)
-        assert (user.get_id() == id)
-        assert (user.email == email)
-        assert (user.password == password)
-        assert (user.role == role)
+        assert(user.get_id() == id)
+        assert(user.email == email)
+        assert(user.password == password)
+        assert(user.role == role)
 
     with nose.allure.step('status'):
-        assert (user.is_active())
-        assert (user.is_authenticated())
-        assert (not user.is_anonymous())
+        assert(user.is_active())
+        assert(user.is_authenticated())
+        assert(not user.is_anonymous())
 
     with nose.allure.step("role"):
         user.role = "customer"
-        assert (user.is_customer())
-        assert (not user.is_admin())
-        assert (not user.is_hotel_admin())
-        assert (not user.is_receptionist())
+        assert(user.is_customer())
+        assert(not user.is_admin())
+        assert(not user.is_hotel_admin())
+        assert(not user.is_receptionist())
 
         user.role = "hotel_admin"
-        assert (user.is_hotel_admin())
-        assert (not user.is_customer())
-        assert (not user.is_admin())
-        assert (not user.is_receptionist())
+        assert(user.is_hotel_admin())
+        assert(not user.is_customer())
+        assert(not user.is_admin())
+        assert(not user.is_receptionist())
 
         user.role = "admin"
-        assert (user.is_admin())
-        assert (not user.is_customer())
-        assert (not user.is_hotel_admin())
-        assert (not user.is_receptionist())
+        assert(user.is_admin())
+        assert(not user.is_customer())
+        assert(not user.is_hotel_admin())
+        assert(not user.is_receptionist())
 
         user.role = "receptionist"
-        assert (user.is_receptionist())
-        assert (not user.is_customer())
-        assert (not user.is_admin())
-        assert (not user.is_hotel_admin())
+        assert(user.is_receptionist())
+        assert(not user.is_customer())
+        assert(not user.is_admin())
+        assert(not user.is_hotel_admin())
 
 
 def test_anonymous_user():
     user = AnonymousUser()
-    assert (not user.is_authenticated())
-    assert (not user.is_active())
-    assert (user.is_anonymous())
-    assert (not user.is_admin())
-    assert (not user.is_hotel_admin())
-    assert (not user.is_receptionist())
-    assert (not user.is_customer())
-    assert (user.get_id() is None)
+    assert(not user.is_authenticated())
+    assert(not user.is_active())
+    assert(user.is_anonymous())
+    assert(not user.is_admin())
+    assert(not user.is_hotel_admin())
+    assert(not user.is_receptionist())
+    assert(not user.is_customer())
+    assert(user.get_id() is None)
 
 
 def test_customer():
@@ -69,11 +68,11 @@ def test_customer():
 
     with nose.allure.step('init customer'):
         customer = Customer(first_name, last_name, email, phone_number, payment_info)
-        assert (customer.first_name == first_name)
-        assert (customer.last_name == last_name)
-        assert (customer.email == email)
-        assert (customer.phone_number == phone_number)
-        assert (customer.payment_info == payment_info)
+        assert(customer.first_name == first_name)
+        assert(customer.last_name == last_name)
+        assert(customer.email == email)
+        assert(customer.phone_number == phone_number)
+        assert(customer.payment_info == payment_info)
 
 
 def test_hotel_admin():
@@ -88,3 +87,4 @@ def test_hotel_admin():
         assert (admin.last_name == last_name)
         assert (admin.email == email)
         assert (admin.phone_number == phone_number)
+
