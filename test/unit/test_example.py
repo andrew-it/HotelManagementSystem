f4c89cfd8528dfdec0162eeb2a813523bee13c8e
Aljutor <aljutor@gmail.com>
2018-04-30 18:44:06 +0300

    Better text example

--- test/unit/test_example.py
+++ test/unit/test_example.py
@@ -1,8 +1,35 @@
 import nose
 
-def test_foo():
+
+@nose.allure.feature("Example Test Feature")
+def test_one():
+    """This is Test Description"""
     with nose.allure.step('step one'):
         assert(True == True)
 
     with nose.allure.step('step two'):
         assert(False == False)
+
+
+@nose.allure.feature("Example Test Feature")
+def test_two():
+    """This is Test Description"""
+    with nose.allure.step('step one'):
+        assert(True == True)
+
+    with nose.allure.step('step two'):
+        assert(False == False)
+
+
+@nose.allure.story("Example Test Story")
+def test_three():
+    """This is Test Description"""
+    with nose.allure.step('step one'):
+        assert(True == True)
+
+
+@nose.allure.story("Example Test Story")
+def test_four():
+    """This is Test Description"""
+    with nose.allure.step('step one'):
+        assert(True == True)
