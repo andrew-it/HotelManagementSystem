8d1fb54ab39364397c6d7cc9ce53fbd81f129427
Mike Lubinets <lubinetsm@yandex.ru>
2018-05-01 17:22:07 +0300

    db classes skeleton

---
+++ app/db.py
@@ -0,0 +1,13 @@
+class Database:
+    def method(self):
+        pass
+
+class PostgresDatabase(Database):
+    def method(self):
+        # TODO
+        pass
+
+class DummyDatabase(Database):
+    def method(self):
+        # TODO
+        pass
