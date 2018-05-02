4976547e09da2a6863c8801dbcc51798eedc3cc9
Andrew <a.ermak@innopolis.ru>
2018-05-02 21:07:45 +0300

    clearup code

--- test/mock_db/test_db.py
+++ test/mock_db/test_db.py
@@ -14,9 +14,7 @@ def test_get_hotels(mock_connect):
                 expected = ['hotel1']
                 mock_connect().cursor.return_value.fetchone.return_value.__getitem__('hotels').return_value = expected
                 result = db.get_all_hotels().return_value
-
-                f.write(str(result) + ' == ' + str(expected))
-            assert (result == expected)
+            assert result == expected
 
 
 @mock.patch("psycopg2.connect")
@@ -28,6 +26,4 @@ def test_get_admins(mock_connect):
                 expected = ['admin1', 'admin2']
                 mock_connect().cursor.return_value.fetchall.return_value = expected
                 result = db.get_all_admins()
-
-                f.write(str(result) + ' == ' + str(expected))
             assert result == expected
