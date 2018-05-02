659dae4dcad46a1b577f6af27c05c4d82cce2df4
Andrew <a.ermak@innopolis.ru>
2018-05-02 17:10:16 +0300

    update

--- test/mock_db/test_db.py
+++ test/mock_db/test_db.py
@@ -1,9 +1,15 @@
+import mock
 import nose
 
 from app.db import AndrewDB
 
 
+@mock.patch("psycopg2.connect")
+@mock.patch("psycopg2.extensions.cursor.fetchall")
 def test_get_hotels():
     with nose.allure.step('Get all hotels'):
         db = AndrewDB()
-        db.get_all_hotels()
+        query_result = None
+        with mock.patch('psycopg2.connect') as mock_connect:
+            mock_connect.cursor.return_value.execute.fetch_all = query_result
+            db.get_all_hotels()
