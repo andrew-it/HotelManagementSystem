1d8dac478bc0f681fefc6e050c8ead6e9470dc7b
Aljutor <aljutor@gmail.com>
2018-05-03 02:10:28 +0300

    Fix ghost errors

--- test/run_flake.sh
+++ test/run_flake.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mkdir -p test/static/result/
+rm -r test/static/result/flake8_junit.xml
 python -m flake8 --output-file test/static/result/flake8.txt app || true
 flake8_junit test/static/result/flake8.txt test/static/result/flake8_junit.xml
--- test/run_mypy.sh
+++ test/run_mypy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p test/static/result/
+rm -r test/static/result/mypy.xml
 python -m mypy app --ignore-missing-imports --junit-xml test/static/result/mypy.xml || true
