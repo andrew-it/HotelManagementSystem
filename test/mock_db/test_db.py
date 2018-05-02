4d95570830b59966a29211afe783096440f5a18b
Andrew <a.ermak@innopolis.ru>
2018-05-02 21:18:44 +0300

    clearup code

--- test/mock_db/test_db.py
+++ test/mock_db/test_db.py
@@ -9,21 +9,19 @@ from app.db import AndrewDB
 def test_get_hotels(mock_connect):
     with allure.step('Get all hotels'):
         with app.app_context():
-            with open('test.log', 'w') as f:
-                db = AndrewDB()
-                expected = ['hotel1']
-                mock_connect().cursor.return_value.fetchone.return_value.__getitem__('hotels').return_value = expected
-                result = db.get_all_hotels().return_value
-            assert result == expected
+            db = AndrewDB()
+            expected = ['hotel1']
+            mock_connect().cursor.return_value.fetchone.return_value.__getitem__('hotels').return_value = expected
+            result = db.get_all_hotels().return_value
+        assert result == expected
 
 
 @mock.patch("psycopg2.connect")
 def test_get_admins(mock_connect):
     with allure.step('Get all hotels'):
         with app.app_context():
-            with open('test.log', 'w') as f:
-                db = AndrewDB()
-                expected = ['admin1', 'admin2']
-                mock_connect().cursor.return_value.fetchall.return_value = expected
-                result = db.get_all_admins()
-            assert result == expected
+            db = AndrewDB()
+            expected = ['admin1', 'admin2']
+            mock_connect().cursor.return_value.fetchall.return_value = expected
+            result = db.get_all_admins()
+        assert result == expected
