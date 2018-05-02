77209e7486a26f1d2fdc734d38450addcae6948c
Andrew <a.ermak@innopolis.ru>
2018-05-02 17:17:33 +0300

    remove annotations

--- test/mock_db/test_db.py
+++ test/mock_db/test_db.py
@@ -4,8 +4,6 @@ import nose
 from app.db import AndrewDB
 
 
-@mock.patch("psycopg2.connect")
-@mock.patch("psycopg2.extensions.cursor.fetchall")
 def test_get_hotels():
     with nose.allure.step('Get all hotels'):
         db = AndrewDB()
