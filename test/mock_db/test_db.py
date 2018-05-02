335484ca96ce1c92a4ea7af9a45a4163bb421671
Andrew <a.ermak@innopolis.ru>
2018-05-02 21:05:24 +0300

    delete unittest usage

--- test/mock_db/test_db.py
+++ test/mock_db/test_db.py
@@ -1,35 +1,32 @@
-import unittest
-
 import mock
 
 from app import app
 from app.db import AndrewDB
 
 
-class TestPatchConn(unittest.TestCase):
+@mock.patch("psycopg2.connect")
+def test_get_hotels(mock_connect):
+    # with nose.allure.step('Get all hotels'):
+    with app.app_context():
+        with open('test.log', 'w') as f:
+            db = AndrewDB()
+            expected = ['sss']
+            mock_connect().cursor.return_value.fetchone.return_value.__getitem__('hotels').return_value = expected
+            result = db.get_all_hotels().return_value
 
-    @mock.patch("psycopg2.connect")
-    def test_get_hotels(self, mock_connect):
-        # with nose.allure.step('Get all hotels'):
-        with app.app_context():
-            with open('test.log', 'w') as f:
-                db = AndrewDB()
-                expected = ['sss']
-                mock_connect().cursor.return_value.fetchone.return_value.__getitem__('hotels').return_value = expected
-                result = db.get_all_hotels().return_value
+            f.write(str(result) + ' == ' + str(expected))
+        assert (result == expected)
 
-                f.write(str(result) + ' == ' + str(expected))
-            self.assertEqual(result, expected)
 
-    @mock.patch("psycopg2.connect")
-    def test_get_admins(self, mock_connect):
-        # with nose.allure.step('Get all hotels'):
-        with app.app_context():
-            with open('test.log', 'w') as f:
-                db = AndrewDB()
-                expected = ['admin1', 'admin2']
-                mock_connect().cursor.return_value.fetchall.return_value = expected
-                result = db.get_all_admins()
+@mock.patch("psycopg2.connect")
+def test_get_admins(mock_connect):
+    # with nose.allure.step('Get all hotels'):
+    with app.app_context():
+        with open('test.log', 'w') as f:
+            db = AndrewDB()
+            expected = ['admin1', 'admin2']
+            mock_connect().cursor.return_value.fetchall.return_value = expected
+            result = db.get_all_admins()
 
-                f.write(str(result) + ' == ' + str(expected))
-            self.assertEqual(result, expected)
+            f.write(str(result) + ' == ' + str(expected))
+        assert result == expected
