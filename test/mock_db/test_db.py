e313df60e7da0495c7c880afd253396c79a1dc90
Andrew <a.ermak@innopolis.ru>
2018-05-02 20:53:01 +0300

    db tests are works!!!

--- test/mock_db/test_db.py
+++ test/mock_db/test_db.py
@@ -1,16 +1,35 @@
+import unittest
+
 import mock
-import nose
 
 from app import app
 from app.db import AndrewDB
 
 
-def test_get_hotels():
-    with nose.allure.step('Get all hotels'):
+class TestPatchConn(unittest.TestCase):
+
+    @mock.patch("psycopg2.connect")
+    def test_get_hotels(self, mock_connect):
+        # with nose.allure.step('Get all hotels'):
         with app.app_context():
-            db = AndrewDB()
-            expected = None
-            with mock.patch('psycopg2.connect') as mock_connect:
-                mock_connect.cursor.return_value.execute.fetch_all = expected
-                result = db.get_all_hotels()
-                assert (result == expected)
+            with open('test.log', 'w') as f:
+                db = AndrewDB()
+                expected = ['sss']
+                mock_connect().cursor.return_value.fetchone.return_value.__getitem__('hotels').return_value = expected
+                result = db.get_all_hotels().return_value
+
+                f.write(str(result) + ' == ' + str(expected))
+            self.assertEqual(result, expected)
+
+    @mock.patch("psycopg2.connect")
+    def test_get_admins(self, mock_connect):
+        # with nose.allure.step('Get all hotels'):
+        with app.app_context():
+            with open('test.log', 'w') as f:
+                db = AndrewDB()
+                expected = ['admin1', 'admin2']
+                mock_connect().cursor.return_value.fetchall.return_value = expected
+                result = db.get_all_admins()
+
+                f.write(str(result) + ' == ' + str(expected))
+            self.assertEqual(result, expected)
