5a981d6f81a99a1ef4170cc43a47f16181163aa5
Andrew <a.ermak@innopolis.ru>
2018-05-02 21:07:14 +0300

    add alure step

--- test/mock_db/test_db.py
+++ test/mock_db/test_db.py
@@ -1,3 +1,4 @@
+import allure
 import mock
 
 from app import app
@@ -6,27 +7,27 @@ from app.db import AndrewDB
 
 @mock.patch("psycopg2.connect")
 def test_get_hotels(mock_connect):
-    # with nose.allure.step('Get all hotels'):
-    with app.app_context():
-        with open('test.log', 'w') as f:
-            db = AndrewDB()
-            expected = ['sss']
-            mock_connect().cursor.return_value.fetchone.return_value.__getitem__('hotels').return_value = expected
-            result = db.get_all_hotels().return_value
+    with allure.step('Get all hotels'):
+        with app.app_context():
+            with open('test.log', 'w') as f:
+                db = AndrewDB()
+                expected = ['hotel1']
+                mock_connect().cursor.return_value.fetchone.return_value.__getitem__('hotels').return_value = expected
+                result = db.get_all_hotels().return_value
 
-            f.write(str(result) + ' == ' + str(expected))
-        assert (result == expected)
+                f.write(str(result) + ' == ' + str(expected))
+            assert (result == expected)
 
 
 @mock.patch("psycopg2.connect")
 def test_get_admins(mock_connect):
-    # with nose.allure.step('Get all hotels'):
-    with app.app_context():
-        with open('test.log', 'w') as f:
-            db = AndrewDB()
-            expected = ['admin1', 'admin2']
-            mock_connect().cursor.return_value.fetchall.return_value = expected
-            result = db.get_all_admins()
+    with allure.step('Get all hotels'):
+        with app.app_context():
+            with open('test.log', 'w') as f:
+                db = AndrewDB()
+                expected = ['admin1', 'admin2']
+                mock_connect().cursor.return_value.fetchall.return_value = expected
+                result = db.get_all_admins()
 
-            f.write(str(result) + ' == ' + str(expected))
-        assert result == expected
+                f.write(str(result) + ' == ' + str(expected))
+            assert result == expected
