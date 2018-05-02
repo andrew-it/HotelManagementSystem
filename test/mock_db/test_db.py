cfe67baa8db7362bbd85d9ed01ea4997343f5069
Andrew <a.ermak@innopolis.ru>
2018-05-02 17:41:42 +0300

    update

--- test/mock_db/test_db.py
+++ test/mock_db/test_db.py
@@ -1,14 +1,16 @@
 import mock
 import nose
 
+from app import app
 from app.db import AndrewDB
 
 
 def test_get_hotels():
     with nose.allure.step('Get all hotels'):
-        db = AndrewDB()
-        expected = None
-        with mock.patch('psycopg2.connect') as mock_connect:
-            mock_connect.cursor.return_value.execute.fetch_all = expected
-            result = db.get_all_hotels()
-            assert (result == expected)
+        with app.app_context():
+            db = AndrewDB()
+            expected = None
+            with mock.patch('psycopg2.connect') as mock_connect:
+                mock_connect.cursor.return_value.execute.fetch_all = expected
+                result = db.get_all_hotels()
+                assert (result == expected)
